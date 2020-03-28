afdf259bca3fe05ed35344a3ebc240b6a60cf154
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2020-03-28 15:17:27 -0300

    Implementação de testes TDD

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const { errors } = require('celebrate');
 const routes = require('./routes');
 
-
 const app = express();
 
 app.use(cors());
@@ -11,4 +10,4 @@ app.use(express.json());
 app.use(routes);
 app.use(errors());
 
-app.listen(3333);
+module.exports = app;
